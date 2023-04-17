5c3aab0a434155d7e8bfe6fc09a997b2f18452b7
Aspen Wilson <aspen.schell@gmail.com>
2023-04-16 20:16:38 -0700

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, {useState, useEffect} from 'react'
+
+function App() {
+    const [dogImage, setDogImage] = useState([])
+    const [imageLoaded, setImageLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then((data) => {
+            setDogImage(data)
+            setImageLoaded(true)
+        })
+    }, [])
+
+    if (!imageLoaded) return <p>Loading...</p>
+  return (
+    <div>
+      <img src={dogImage.message} alt='A Random Dog' />
+    </div>
+  )
+}
+export default App
